addc41b73be4cba81d6c037d8ee59501b9096f0b
Colin Johnson <blisk76@gmail.com>
2020-03-14 18:43:16 -0400

    reorginizing

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,16 @@
-var secondsDisplay = document.querySelector("#seconds");
-
-var container = document.querySelector("#startingPage");
-
-var multipleChoice = [
+var questions = [
   {
-    question: "Commonly used data types do NOT include:",
+    questionTitle: "Commonly used data types do NOT include:",
     choices: ["strings( )", "booleans( )", "alerts( )", "numbers( )"],
     correctAnswer: "alerts( )"
   },
   {
-    question: "The condition of an if/else statement is closed within:",
+    questionTitle: "The condition of an if/else statement is closed within:",
     choices: ["commas( )", "brackets( )", "parentheses( )", "braces( )"],
     correctAnswer: "parentheses( )"
   },
   {
-    question: "Arrays in Javascript can be used to store:",
+    questionTitle: "Arrays in Javascript can be used to store:",
     choices: [
       "Numbers and strings( )",
       "booleans( )",
@@ -24,12 +20,12 @@ var multipleChoice = [
     correctAnswer: "All of the Above( )"
   },
   {
-    question: "Javascript was first released in what year?",
+    questionTitle: "Javascript was first released in what year?",
     choices: ["1993( )", "1996( )", "1998( )", "1995( )"],
     correctAnswer: "1995( )"
   },
   {
-    question:
+    questionTitle:
       "Which of these browsers is no longer supported by nearly developer?",
     choices: ["Chrome( )", "Netscape( )", "Edge( )", "Firefox( )"],
     correctAnswer: "Netscape( )"
@@ -37,66 +33,3 @@ var multipleChoice = [
 ];
 
 
-
-function timerStart() {
-  var secondsLeft = 75;
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    secondsDisplay.textContent = secondsLeft;
-
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-      finalScreen();
-    }
-  }, 1000);
-  questionLoop()
-}
-
-
-
-
-function gameOver() {
-    clearInterval(timerInterval);
-    var endScreen = "<h2>Game Over!</h2> <Your time left was>" + secondsLeft + "</h3> <input type= 'text' id='Initials' placeholder='initials'><button onclick='recordScore()'>Record Your Score!</button>";
-
-    document.getElementById("mainPage").innerHTML = endScreen;
-}
-
-
-function wrong() {
-  timeLeft -= 15;
-  questionLoop();
-}
-
-function questionLoop() {
-  questions++;
-
-  if (questions > multipleChoice.length) {
-    gameOver();
-    return;
-  }
-
-  var quiz = "<h1>" + multipleChoice[questions].title + "</h1>";
-
-  for (i = 0; i < multipleChoice[questions].choices.length; i++) {
-    var buttonSelect = 'button onclick="[ANS}">[CHOICE]</button>';
-    buttonCode = buttonCode.replace(
-      "[CHOICE]",
-      questions[currentQuestion].choices[buttonLoop]
-    );
-    if (
-      questions[currentQuestion].choices[buttonLoop] ==
-      questions[currentQuestion].answer
-    ) {
-      buttonCode = buttonCode.replace("[ANS]", "correct()");
-    } else {
-      buttonCode = buttonCode.replace("[ANS]", "incorrect()");
-    }
-    quizContent += buttonCode;
-  }
-
-  document.getElementById(".startingPage").innerHTML = quizContent;
-}
-
-function showScores() {}
-$("#startBtn").on("click", timerStart());

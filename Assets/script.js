74092be39df522b63b3f6ea81feb2a694dd7db9a
Colin Johnson <blisk76@gmail.com>
2020-03-14 16:54:06 -0400

    wrote question array, start question loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,76 @@
-var secondsDisplay = document.querySelector("#seconds"); 
-var secondsLeft = 75
-var container = document.querySelector("#startingPage")
+var secondsDisplay = document.querySelector("#seconds");
 
+var container = document.querySelector("#startingPage");
 
+var multipleChoice = [
+  {
+    question: "Commonly used data types do NOT include:",
+    choices: ["strings( )", "booleans( )", "alerts( )", "numbers( )"],
+    correctAnswer: "alerts( )"
+  },
+  {
+    question: "The condition of an if/else statement is closed within:",
+    choices: ["commas( )", "brackets( )", "parentheses( )", "braces( )"],
+    correctAnswer: "parentheses( )"
+  },
+  {
+    question: "Arrays in Javascript can be used to store:",
+    choices: [
+      "Numbers and strings( )",
+      "booleans( )",
+      "other Arrays( )",
+      "All of the Above( )"
+    ],
+    correctAnswer: "All of the Above( )"
+  },
+  {
+    question: "Javascript was first released in what year?",
+    choices: ["1993( )", "1996( )", "1998( )", "1995( )"],
+    correctAnswer: "1995( )"
+  },
+  {
+    question:
+      "Which of these browsers is no longer supported by nearly developer?",
+    choices: ["Chrome( )", "Netscape( )", "Edge( )", "Firefox( )"],
+    correctAnswer: "Netscape( )"
+  }
+];
 
+function timerStart() {
+  var secondsLeft = 75;
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    secondsDisplay.textContent = secondsLeft;
 
-function timerStart(){
-    var timerInterval = setInterval(function(){
-        secondsLeft--;
-        secondsDisplay.textContent = secondsLeft;
-
-        if(secondsLeft === 0){
-            
-        }
-    }, 1000);
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      finalScreen();
+    }
+  }, 1000);
 }
 
-function showScores() {
-    
+
+function questionLoop(){
+    questions++;
+
+    if (questions > multipleChoice.length){
+        "endgame";
+        return;
+    }
+
+
+    var quiz = "<h1>" + multipleChoice[questions].title "</h1>"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function showScores() {}
